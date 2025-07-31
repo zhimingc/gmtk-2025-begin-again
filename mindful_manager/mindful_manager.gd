b7d5600ae3c2fc572bbb81f6053d42c6f610a47e
Zhiming Chen <zhi.m.ch@gmail.com>
2025-07-31 18:13:52 +0800

    Added press input and good/bad press detection

--- mindful_manager/mindful_manager.gd
+++ mindful_manager/mindful_manager.gd
@@ -1,8 +1,9 @@
 extends Node
 
 @export var press_frequency : float = 4.0
+@export var press_window : float = 0.25
 
-@onready var mindful_viz : MindfulVisualizer = %MindfulVisualizer
+@onready var mindful_vis : MindfulVisualizer = %MindfulVisualizer
 
 var mindful_timer : float = 0.0
 
@@ -11,11 +12,21 @@ func _ready() -> void:
 
 func restart_mindful_timer() -> void:
 	mindful_timer = press_frequency
+	mindful_vis.loop_visualization()
 
 func _process(delta: float) -> void:
 	mindful_timer -= delta
 	if mindful_timer <= 0.0:
-		mindful_viz.loop_visualization()
 		restart_mindful_timer()
-	mindful_viz.update_visualization(mindful_timer/press_frequency)
-		
+	mindful_vis.update_visualization(get_mindful_press_progress())
+
+	if Input.is_action_just_pressed("mindful_input"):
+		# check if the press is within 0.0 +/- press window
+		if mindful_timer < press_window or press_frequency - mindful_timer < press_window:
+			mindful_vis.trigger_good_press()
+		else:
+			mindful_vis.trigger_bad_press()
+			restart_mindful_timer()
+
+func get_mindful_press_progress() -> float:
+	return mindful_timer/press_frequency
--- mindful_manager/mindful_visualizer.gd
+++ mindful_manager/mindful_visualizer.gd
@@ -1,12 +1,28 @@
 class_name  MindfulVisualizer extends Node2D
 
 @onready var timer_circle : Sprite2D = $TimerCircle
+@onready var center_vis : Sprite2D = $Center
+@onready var good_press_parts : CPUParticles2D = $GoodPressParticles
+@onready var bad_press_parts : CPUParticles2D = $BadPressParticles
 
 func update_visualization(progress : float) -> void:
 	timer_circle.scale = Vector2.ONE * progress
 
 func loop_visualization() -> void:
-	timer_circle.scale = Vector2.ONE
-	timer_circle.modulate = Color.TRANSPARENT
+	if timer_circle:
+		timer_circle.scale = Vector2.ONE
+		timer_circle.modulate = Color.TRANSPARENT
+		var timer_circle_fade = create_tween()
+		timer_circle_fade.tween_property(timer_circle, "modulate", Color.WHITE, 0.75)
+
+func trigger_good_press() -> void:
+	# center_vis.modulate = Color.CHARTREUSE
+	# var center_mod = create_tween()
+	# center_mod.tween_property(center_vis, "modulate", Color.WHITE, 1.0)
+	good_press_parts.emitting = true
+
+func trigger_bad_press() -> void:
+	bad_press_parts.emitting = true
+	timer_circle.self_modulate = Color.DARK_RED
 	var timer_circle_fade = create_tween()
-	timer_circle_fade.tween_property(timer_circle, "modulate", Color.WHITE, 0.75)
+	timer_circle_fade.tween_property(timer_circle, "self_modulate", Color.WHITE, 1.25)
